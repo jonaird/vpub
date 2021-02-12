8be645f50cbac1f294d9ea1c624baec89b4c3eeb
Jonathan Aird <jonathan.aird@gmail.com>
2021-02-12 11:57:19 +0200

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "2582586fd804b6a59896c2a25e1b91f2",
-"index.html": "71b2f679bd7dcabd78975bf2b9d7af05",
-"/": "71b2f679bd7dcabd78975bf2b9d7af05",
-"main.dart.js": "d34cac93ab0b56e59565b023905cb801",
+"index.html": "f76d0b69dbfd53092c41f1b6efc9da4b",
+"/": "f76d0b69dbfd53092c41f1b6efc9da4b",
+"main.dart.js": "b7664828636fe1e6c409cf6800f23bfc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
